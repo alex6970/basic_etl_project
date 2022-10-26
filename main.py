bb48a3e6d0b5eff1f26e65680c017f5784786139
Goncalves <alexandra.goncalves@dxc.com>
2022-10-26 17:00:47 +0200

    Fixed extraction part

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ import requests
 import datetime
 import pandas as pd
 
+pd.set_option('display.max_columns', None)
+pd.set_option('display.max_rows', None)
+
+
 USER_ID = "11135754916"
 TOKEN = "BQCrXOzqVAReebAnOB31mx7RZtSe8ayLe6iuQN_l76C47otd2gnd02O8LJZKdax64HtEK0TxVVpvaTOPV8-ZcMGVPREt6PWxTktN3FB-nUn89AByYMPqw0Jd-Bv57WlQ1G98TQAyHXg9c1rePRt-_k85g-WCGgbwKB0tAC9dZrej28q-n96r2g"  # token expires after 3600 s
 
@@ -13,47 +17,52 @@ headers = {
 
 # over the last 24 hours
 today = datetime.datetime.now()
-yesterday = today - datetime.timedelta(days=1)
+yesterday = today - datetime.timedelta(days=2)
+
 
 # converting to unix milliseconds, as accepted by the API
 yesterday_unix = int(yesterday.timestamp()) * 1000 
 
-req = requests.get("https://api.spotify.com/v1/me/player/recently-played?after={time}".format(time=yesterday_unix), headers = headers)
+# request (up to 50 songs, max from spotify API)
+req = requests.get("https://api.spotify.com/v1/me/player/recently-played?limit=50&after={time}".format(time=yesterday_unix), headers = headers)
 data = req.json()
 
 # print(data)
+
+for x in data["items"] : 
+    print(x["track"]["name"])
 # print(data["items"][:2])
 
-song_name = []
-album_name = []
-artist = []
-played_at = []
-tmstp = []
+# song_name = []
+# album_name = []
+# artist = []
+# played_at = []
+# tmstp = []
 
-for item in data["items"] : 
-    song_name.append(item["track"]["name"])
-    album_name.append(item["track"]["album"]["name"])
-    artist.append(item["track"]["artists"][0]["name"])
-    played_at.append(item["played_at"])
-    tmstp.append(item["played_at"][:10])
+# for item in data["items"] : 
+#     song_name.append(item["track"]["name"])
+#     album_name.append(item["track"]["album"]["name"])
+#     artist.append(item["track"]["artists"][0]["name"])
+#     played_at.append(item["played_at"])
+#     tmstp.append(item["played_at"][:10])
 
 
-# for x in range(2):
-#     print(song_name[x])
-#     print(album_name[x])
-#     print(artist[x])
-#     print(played_at[x])
-#     print(tmstp[x])
-#     print("\n")
+# # for x in range(2):
+# #     print(song_name[x])
+# #     print(album_name[x])
+# #     print(artist[x])
+# #     print(played_at[x])
+# #     print(tmstp[x])
+# #     print("\n")
 
-df = pd.DataFrame(columns=["Song", "Artist", "Album", "Played at", "Timestamp"])
+# df = pd.DataFrame(columns=["Song", "Artist", "Album", "Played at", "Timestamp"])
 
-df["Song"] = song_name
-df["Artist"] = artist
-df["Album"] = album_name
-df["Played at"] = played_at
-df["Timestamp"] = tmstp
+# df["Song"] = song_name
+# df["Artist"] = artist
+# df["Album"] = album_name
+# df["Played at"] = played_at
+# df["Timestamp"] = tmstp
 
-print(df.head())
+# print(df.head())
 
-# TODO : github + extract done-> transform part
+# # TODO : github + extract done-> transform part
